dd26210c5cc76e5c28e6aff9f83867d541d32218
Mahesh <maheshrlokhande94@gmail.com>
2021-08-30 21:17:28 -0400

    results stored

--- cpp-homeworks/homework_1/task_2/build.sh
+++ cpp-homeworks/homework_1/task_2/build.sh
@@ -1,5 +1,5 @@
 mkdir build  //make dir
-c++ -c -I./include/ -Idir/ src/sum.cpp -o build/sum.o                 //compile sum.cpp
-c++ -c -I./include/ -Idir/ src/subtract.cpp -o build/subtract.o         //compile subtract.cpp
-c++ -std=c++17 -I./include/ src/main.cpp -L ./build -lipb_arithmetic -o build/test_ipb_arithmetic           //link main to library created and
-
+c++ -c -I./include/ -Idir/ src/sum.cpp -o build/sum.o                 #compile sum.cpp
+c++ -c -I./include/ -Idir/ src/subtract.cpp -o build/subtract.o         #compile subtract.cpp
+c++ -std=c++17 -I./include/ src/main.cpp -L ./build -lipb_arithmetic -o build/test_ipb_arithmetic           #link main to library created 
+c++ -c -I./include/ -Idir/ src/main.cpp -o build/main.o     #compile main.cpp

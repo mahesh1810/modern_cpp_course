b852d7a57974f022255b383339644040e04c7fe3
Mahesh <maheshrlokhande94@gmail.com>
2022-01-22 19:19:59 -0800

    converted to a catkin workspace

--- cpp-homeworks/homework_1/task_1/commands.sh
+++
@@ -1,4 +0,0 @@
-wc -l <data.dat 
-grep -c 'dolor\|dalor' data.dat 
-wc -w <data.dat
-grep -c -w '\bmol\w*' data.dat
--- cpp-homeworks/homework_1/task_2/build.sh
+++
@@ -1,9 +0,0 @@
-mkdir build  //make dir
-c++ -c -I./include/ -Idir/ src/sum.cpp -o build/sum.o                 #compile sum.cpp
-c++ -c -I./include/ -Idir/ src/subtract.cpp -o build/subtract.o         #compile subtract.cpp
-c++ -std=c++17 -I./include/ src/main.cpp -L ./build -lipb_arithmetic -o build/test_ipb_arithmetic           #link main to library created 
-c++ -c -I./include/ -Idir/ src/main.cpp -o build/main.o     #compile main.cpp
-
-#results
-cp build/test_ipb_arithmetic results/bin
-cp build/libipb_arithmetic.a results/lib/
